bfe01c8a5e3efb7e22960d685b66e5eca9fae3df
akonkol <825835+akonkol@users.noreply.github.com>
2024-05-27 11:07:02 -0500

    bold hostname

--- bin/Screens.py
+++ bin/Screens.py
@@ -424,20 +424,19 @@ class SummaryScreen(BaseScreen):
         uptime_in_seconds = self.utils.shell_cmd("cat /proc/uptime | cut -d' ' -f1")
 
         self.display.prepare()
-        big_font = self.font(20)
+        font_size = 20
+        big_font = self.font(font_size, is_bold=True)
         small_font = self.font(10)
         text = self.text
 
         self.logger.info("Rendering summary text: " + text)
         hostname_text_width, hostname_text_height = Utils.get_text_size(self.display, hostname.lower(), big_font)
-        font_size = 20
         while hostname_text_width > self.display.width:
           font_size -= 1
           big_font = self.font(font_size)
           hostname_text_width, hostname_text_height = Utils.get_text_size(self.display, hostname.lower(), big_font)
 
         self.display.draw.text( (0, -2), hostname.lower(), font=big_font, fill=255)
-
         self.display.draw.text((x, top + 24), ipv4, font=small_font, fill=255)
         self.display.draw.text((width - 35 , top + 24), self.utils.display_time(float(uptime_in_seconds.strip()), 4), font=small_font, fill=255)
 
